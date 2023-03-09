d3eaf56c05a7ce193dc42d06a0f1e59ab44baa40
Ethan Charoenpitaks <echaroenpit6012@stu.hinsdale86.org>
2023-03-09 03:21:54 +0000

    Reduce pen width

--- src/renderer/renderElement.ts
+++ src/renderer/renderElement.ts
@@ -1374,7 +1374,7 @@ export function getFreeDrawSvgPath(element: ExcalidrawFreeDrawElement) {
   // Consider changing the options for simulated pressure vs real pressure
   const options: StrokeOptions = {
     simulatePressure: element.simulatePressure,
-    size: element.strokeWidth * 4.25,
+    size: element.strokeWidth * 1,
     thinning: 0.6,
     smoothing: 0.5,
     streamline: 0.5,
